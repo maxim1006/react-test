db779ee804c118dd2cad10fde0bd01515f38ba29
pansonic2 <thehoteinstein@gmail.com>
2020-03-27 11:17:54 +0500

    1. Added a color to color.scss. 2. Created a mixin (list-block). 3. Created a list component (js+scss) 4. Added a list to app.component. js. Issue: list is not a list on mobile

--- src/app.component.js
+++ src/app.component.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import './app.component.scss';
+import React from "react";
+import "./app.component.scss";
 import Header from "./components/header/header.component";
+import List from "./components/list/list.component";
 
 function AppComponent() {
-    return (
-        <div className="App">
-            <Header />
+  return (
+    <div className="App">
+      <Header />
 
-            <main>
-                Hello world text
-            </main>
-        </div>
-
-    );
+      <main>
+        Hello world text
+        <List>
+          <li>123</li>
+          <li>456</li>
+          <li>789</li>
+        </List>
+      </main>
+    </div>
+  );
 }
 
 export default AppComponent;
-
---
+++ src/components/list/list.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./list.component.scss";
+
+export default class List extends Component {
+  render() {
+    return (
+      <ul className="list">
+        <li>123</li>
+        <li>456</li>
+        <li>789</li>
+      </ul>
+    );
+  }
+}
