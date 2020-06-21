93c62896b478969613f8e244a7a3255283ef0cbd
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-06-21 12:55:30 +0500

    add-material-loader

--- src/components/suspense/suspense.component.ts
+++
@@ -1,4 +0,0 @@
-// TODO instead of string ("Loading") return material loader
-const SuspenseComponent: any = () => "Loading...";
-
-export default SuspenseComponent;
---
+++ src/components/suspense/suspense.component.tsx
@@ -0,0 +1,8 @@
+// TODO instead of string ("Loading") return material loader
+
+import React from "react";
+import Loader from "./loader";
+
+const SuspenseComponent = () => <Loader />;
+
+export default SuspenseComponent;
