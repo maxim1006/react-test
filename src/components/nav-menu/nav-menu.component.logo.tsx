6fcd8f67666ea3f10e047a1bd215e526c5195cf5
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-06-24 13:00:34 +0500

    turn new components into styled

--- src/components/nav-menu/nav-menu.component.logo.tsx
+++ src/components/nav-menu/nav-menu.component.logo.tsx
@@ -1,7 +1,24 @@
+// styled component
 import React from "react";
-import "./nav-menu.component.logo.scss";
 import twitterlogo from "../../assets/images/twitter-logo.png";
 
-const logo = () => <img className="reactLogo" src={twitterlogo} alt="123" />;
+type logo = {
+    width: number;
+    height: number;
+    transform: string;
+    fontSize: number;
+};
+
+const logo = ({ width = 50, height = 50, transform = 5, fontSize = 10 }) => {
+    const styles = {
+        width: `${width}px`,
+        height: `${height}px`,
+        transform: `translateY(-${transform}px)}`,
+        fontSize: `${fontSize}px`,
+    };
+    return (
+        <img style={styles} className="reactLogo" src={twitterlogo} alt="123" />
+    );
+};
 
 export default logo;
--- src/components/nav-menu/nav-menu.component.title.tsx
+++ src/components/nav-menu/nav-menu.component.title.tsx
@@ -1,6 +1,27 @@
-import React from "react";
-import "./nav-menu.component.title.scss";
+// styled component
+// had to use 'flex' instead of 'flexGrow (flex-grow)'
+// transform does not do anything
 
-const Title = () => <span className="reactTitle">REACT PROJECT</span>;
+import React from "react";
+
+type logo = {
+    transform: string;
+    fontSize: number;
+    flex: number;
+};
+
+const Title = ({ transformY = 8, fontSize = 24, flex = 1 }) => {
+    const styles = {
+        transform: `translateY(-${transformY})}px`,
+        fontSize: `${fontSize}px`,
+        flex: `${flex}`,
+    };
+    return (
+        <span className="reactTitle" style={styles}>
+            {" "}
+            REACT PROJECT
+        </span>
+    );
+};
 
 export default Title;
