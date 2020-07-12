7eb46712343c2576f5c2b87924e6aadec9ccd334
max <maxim.maximov@netcracker.com>
2020-07-12 18:58:12 +0300

    add proper svg

---
+++ src/components/bem/bem.component.tsx
@@ -0,0 +1,19 @@
+import React, { memo } from 'react';
+import './bem.component.scss';
+
+const Bem: React.FC = () => {
+    return (
+        <div className="bem _with-big-element">
+            <div className="bem__element">Element</div>
+            <div className="bem__element _big">Element big</div>
+            <ul className="bem__list">
+                <li className="bem__list-item">List Item</li>
+            </ul>
+            <a href="#" className="bem__link">
+                Element 3
+            </a>
+        </div>
+    );
+};
+
+export default memo(Bem);
