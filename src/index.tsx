13fbcdfbd9b4e3fe5c54865d2c3c24af4b23014b
max <maxim.maximov@netcracker.com>
2020-07-12 18:08:36 +0300

    rebase example

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import AppComponent from "./app.component";
 
-console.log("some change");
-
 ReactDOM.render(<AppComponent />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
