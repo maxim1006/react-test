ef6fe7e6bd54297238d9b5418112b52f9ba9797c
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-08-26 13:33:29 +0500

    Remove unused useState

--- src/components/popovers/popover-component.js
+++ src/components/popovers/popover-component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
