dc32f36ca16a4f4e8695fcfa66b082ebda4bfe59
Max Maximov <maxim.maximov@netcracker.com>
2020-11-01 18:53:52 +0300

    Update

---
+++ src/components/hook-on-start/hook-on-start.component.tsx
@@ -0,0 +1,37 @@
+import React, { FC, memo, useEffect, useState } from 'react';
+
+type HookOnStartProps = {};
+
+let intervalId: number;
+
+const HookOnStart: FC<HookOnStartProps> = memo<HookOnStartProps>(() => {
+    let [counter, setCounter] = useState(0);
+
+    if (counter === 10) {
+        clearInterval(intervalId);
+    }
+
+    useEffect(() => {
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const onClick = () => {
+        if (counter !== 0 && counter < 10) return;
+
+        setCounter(0);
+
+        intervalId = window.setInterval(() => {
+            setCounter(i => {
+                return ++i;
+            });
+        }, 1000);
+    };
+
+    return (
+        <div onClick={onClick} style={{ color: '#fff' }}>
+            HookOnStart {counter}
+        </div>
+    );
+});
+
+export default HookOnStart;
