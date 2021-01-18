95f9a3d1270c7f4d09feed7d6aa563b9a6369095
StanislavAlexandrov <StanislavAlexandrov@users.noreply.github.com>
2021-01-18 17:37:43 +0000

    Deploying to gh-pages from  @ c8245b9ce317f60be92ff2b8c8a80e82fa80f506 🚀

---
+++ precache-manifest.a42a3a53d8d315f9d31a27980c83cff6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd284f5fa6d19d5e8bacd577fce1936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2df975d759ca85aa698c",
+    "url": "/static/css/3.7a802669.chunk.css"
+  },
+  {
+    "revision": "6b527e2a80961e417a39",
+    "url": "/static/css/main.78a44b22.chunk.css"
+  },
+  {
+    "revision": "27a7fa1927d3d6a42512",
+    "url": "/static/js/2.3c7a49e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3c7a49e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df975d759ca85aa698c",
+    "url": "/static/js/3.5c30e845.chunk.js"
+  },
+  {
+    "revision": "0b1b117218db2452e63c",
+    "url": "/static/js/4.a0baeeec.chunk.js"
+  },
+  {
+    "revision": "cc2e177df764988c4149",
+    "url": "/static/js/5.621c2014.chunk.js"
+  },
+  {
+    "revision": "2788599d2cdd563edc9b",
+    "url": "/static/js/6.9903b055.chunk.js"
+  },
+  {
+    "revision": "6b527e2a80961e417a39",
+    "url": "/static/js/main.bffb8223.chunk.js"
+  },
+  {
+    "revision": "2a1649810adbe7f86569",
+    "url": "/static/js/runtime-main.af681d06.js"
+  },
+  {
+    "revision": "96e43b426543a3bcb33ce6ac91829bfc",
+    "url": "/static/media/logo.96e43b42.svg"
+  }
+]);
